a4b704e814a9f1b0f337291d594b8dd47bb11ed0
pallav.sidana <pallavsidana@gmail.com>
2021-02-15 16:04:27 +0530

    aded code in the checkbox.java

--- FarmeworkPractise/src/test/java/com/frameworkpractise/checkbox/Checkboxes.java
+++ FarmeworkPractise/src/test/java/com/frameworkpractise/checkbox/Checkboxes.java
@@ -6,18 +6,17 @@ import org.openqa.selenium.support.FindBy;
 
 import com.frameworkpractise.base.BasePageObjects;
 
-public class Checkboxes extends BasePageObjects  {
+public class Checkboxes extends BasePageObjects {
 
-
-	//Click on the check box link
-	@FindBy (xpath="//*[contains(text(),'Checkboxes')]")
+	// Click on the check box link
+	@FindBy(xpath = "//*[contains(text(),'Checkboxes')]")
 	WebElement cBoxes;
-	
-	
+
 	public Checkboxes(WebDriver driver) {
 		super(driver);
 		// TODO Auto-generated constructor stub
 	}
+
 	public void cbox() {
 
 		System.out.println("3.1");
