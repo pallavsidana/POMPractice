00c5e7a08b6af3837cd04c8d51c0c2288a0c8d44
pallav.sidana <pallavsidana@gmail.com>
2021-02-17 21:15:17 +0530

    stash

---
+++ FarmeworkPractise/src/main/java/com/frameworkpractise/pom/pages/StashClass.java
@@ -0,0 +1,12 @@
+package com.frameworkpractise.pom.pages;
+
+import org.testng.annotations.Test;
+
+public class StashClass {
+
+	
+	@Test
+	public void Stashing() {
+		System.out.println("i am doing stash this code");
+	}
+}
